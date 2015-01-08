23ffca1df358d8aae3b241d785e9e99b30efa9c6
Hongxin Zhang <jackson.zhang.828@gmail.com>
2015-01-08 17:07:57 -0500

    First push with designed header, side bar and summary buttons

---
+++ multi-views/scripts/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name StudyMultiViewsApp
+ * @description
+ * # StudyMultiViewsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('StudyMultiViewsApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function () {});
---
+++ multi-views/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name StudyMultiViewsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the StudyMultiViewsApp
+ */
+angular.module('StudyMultiViewsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
